f55c32b590451ee0ba836f16848786ae2c475aed
Ayşenur <aysenur_can_@hotmail.com>
2023-11-15 16:15:27 +0300

    Register page created

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Calculator from './bmi/Calculator'
 import Login from './Login'
+import Register from './Register'
 
 
 function Main() {
   return (
     <div className='grid h-screen place-items-center mt-[-10rem]'>
-    <Login/>
+    <Register/>
     
     </div>
   )
